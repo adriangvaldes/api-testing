f0bf03cc535df32cc8fd6254641404f8b4fd2e64
Adrian Valde <adriangvaldes@gmail.com>
2024-05-19 23:02:19 -0300

    add: Find by id, Delet post, update post

--- controllers/postController.go
+++ controllers/postController.go
@@ -7,7 +7,14 @@ import (
 )
 
 func PostsCreate(c *gin.Context) {
-	post := models.Post{Title: "First post", Body: "Post body"}
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	post := models.Post{Title: body.Title, Body: body.Body}
 
 	result := initializers.DB.Create(&post)
 
@@ -19,4 +26,56 @@ func PostsCreate(c *gin.Context) {
 	c.JSON(200, gin.H{
 		"post": post,
 	})
+}
+
+func PostIndex(c *gin.Context) {
+	// Get the posts
+	var posts []models.Post
+
+	initializers.DB.Find(&posts)
+
+	c.JSON(200, gin.H{
+		"posts": posts,
+	})
+}
+
+func PostsShow(c *gin.Context) {
+	id := c.Param("id")
+
+	var post models.Post
+
+	initializers.DB.First(&post, id)
+
+	c.JSON(200, gin.H{
+		"post": post,
+	})
+}
+
+func PostsUpdate(c *gin.Context) {
+	id := c.Param("id")
+
+	var body struct {
+		Body  string
+		Title string
+	}
+
+	c.Bind(&body)
+
+	var post models.Post
+	initializers.DB.Find(&post, id)
+
+	initializers.DB.Model(&post).Updates(models.Post{
+		Title: body.Title,
+		Body:  body.Body,
+	})
+
+	c.JSON(200, "Post"+id+" sucessfully updated!")
+}
+
+func PostsDelete(c *gin.Context) {
+	id := c.Param("id")
+
+	initializers.DB.Delete(&models.Post{}, id)
+
+	c.Status(200)
 }
